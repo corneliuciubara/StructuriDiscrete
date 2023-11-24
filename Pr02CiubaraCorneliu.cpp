72eb3a45de036fda4bd62f83acec932d94550c65
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2023-11-24 10:35:42 +0200

    Update Pr02CiubaraCorneliu.cpp

--- Pr02CiubaraCorneliu.cpp
+++ Pr02CiubaraCorneliu.cpp
@@ -1,64 +1,49 @@
 #include <iostream>
-
 using namespace std;
 
-long unsigned FactorialIterativ(int n)
-{
+long unsigned FactorialIterativ(int n) {
     long unsigned fact = 1;
-    for(int i=1; i<=n; i++)
-        fact = fact * i;
-        // ALT fact *= i;
+    for(int i = 1; i <= n; i++)
+        fact *= i;
     return fact;
 }
 
-long unsigned FactorialRecursiv(int n)
-{
-    if(n<=1) return 1;
-    else return FactorialRecursiv(n-1)*n;
-}
-long long unsigned Permutari(int n)
-{
-    return FactorialIterativ(n);
-}
-long long unsigned Aranjamente(int n,int m)
-{
-   // return FactorialIterativ(n)/FactorialIterativ(n-m);
-   long long unsigned p = 1;
-   for (int i=n; i>n-m; i--){
-    p *= i;
-   }
-   return p;
-}
-long long unsigned Combinari(int n,int m)
-{
-   long long unsigned p = 1, q = 1 ;
- for(int i =2 ;i<=m; i++){
-        q*= i+1;
-    p *=(n-i+1);
-   }
-   return p*n/q;
-}
-int main()
-{
-    int j,n = 100,m = 98;
-    // cout << "Nr total "; cin >> n;
-   //  cout << "Cate ? "; cin >> n;
-    cout << "Proiectul nr. 2 al studentului Oala Oleg, grupa IS11Z. Elemente de combinatorica" << endl << endl;
-   cout << "Elemente din combinatorica" << endl;
-    cout << "Factorial iterativ" << endl;
-    for(j=0; j<=n; j++);
-    {
-        cout << j << "! = " << FactorialIterativ(j) << endl;
-    }
-    cout << endl; cout << "Factorial recursiv" << endl;
-    for(j=0; j<=n; j++);
-    {
-        cout << j << "! = " << FactorialRecursiv(j) << endl;
-    }
-    cout << "\n Permutari din " <<n << " elemente cate "<< Permutari(n);
-    cout << "\n Aranjamente din " <<n << " elemente cate "<< m << " elemente "<< Aranjamente(n,m);
-    cout << "\n Combinari din " <<n << " elemente cate "<< m << " elemente "<< Combinari(n,m);
-    return 0;
+long unsigned FactorialRecursiv(int n) {
+    if(n <= 1) return 1;
+    else return FactorialRecursiv(n - 1) * n;
 }
 
-// Variante
+long long unsigned Permutari(int n) {
+    return FactorialIterativ(n);
+}
+
+long long unsigned Aranjamente(int n, int m) {
+    return FactorialIterativ(n) / FactorialIterativ(n - m);
+}
+
+long long unsigned Combinari(int n, int m) {
+    return FactorialIterativ(n) / (FactorialIterativ(m) * FactorialIterativ(n - m));
+}
+
+int main() {
+    int n = 100, m = 98;
+
+    cout << "Proiectul nr. 2 al studentului Oala Oleg, grupa IS11Z. Elemente de combinatorica" << endl << endl;
+    cout << "Elemente din combinatorica" << endl;
+    
+    cout << "Factorial iterativ" << endl;
+    for(int j = 0; j <= n; j++) {
+        cout << j << "! = " << FactorialIterativ(j) << endl;
+    }
+
+    cout << endl << "Factorial recursiv" << endl;
+    for(int j = 0; j <= n; j++) {
+        cout << j << "! = " << FactorialRecursiv(j) << endl;
+    }
+
+    cout << "\nPermutari din " << n << " elemente: " << Permutari(n) << endl;
+    cout << "Aranjamente din " << n << " elemente cate " << m << " elemente: " << Aranjamente(n, m) << endl;
+    cout << "Combinari din " << n << " elemente cate " << m << " elemente: " << Combinari(n, m) << endl;
+
+    return 0;
+}
