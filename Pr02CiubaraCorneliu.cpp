fa23294f7957d71fca6c7010df789f6fec5567c8
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2023-12-05 10:16:16 +0200

    Update Pr02CiubaraCorneliu.cpp

--- Pr02CiubaraCorneliu.cpp
+++ Pr02CiubaraCorneliu.cpp
@@ -1,49 +1,98 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-long unsigned FactorialIterativ(int n) {
+long unsigned FactorialIterativ(int n)
+{
     long unsigned fact = 1;
-    for(int i = 1; i <= n; i++)
-        fact *= i;
+    for(int i=1; i<=n; i++)
+        fact = fact * i;
     return fact;
 }
 
-long unsigned FactorialRecursiv(int n) {
-    if(n <= 1) return 1;
-    else return FactorialRecursiv(n - 1) * n;
+long unsigned FactorialRecursiv(int n)
+{
+    if(n<=1) return 1;
+    else return FactorialRecursiv(n-1)*n;
 }
 
-long long unsigned Permutari(int n) {
+long long unsigned Permutari(int n)
+{
     return FactorialIterativ(n);
 }
 
-long long unsigned Aranjamente(int n, int m) {
-    return FactorialIterativ(n) / FactorialIterativ(n - m);
+long long unsigned Aranjamente(int n,int m)
+{
+    long long unsigned p = 1;
+    for(int i=n; i>n-m; i--)
+    {
+        p*=i;
+    }
+    return p;
 }
 
-long long unsigned Combinari(int n, int m) {
-    return FactorialIterativ(n) / (FactorialIterativ(m) * FactorialIterativ(n - m));
+long long unsigned Combinari(int n, int m)
+{
+    long long unsigned p = 1, q = 1;
+    for(int i=2; i<=m; i++)
+    {
+        q*=i;
+        p*=(n-i+1);
+    }
+    return p*n/q;
 }
 
-int main() {
-    int n = 100, m = 98;
+long long unsigned PermutariCuRepetitii(int n)
+{
+    return pow(n,n);
+}
 
+long long unsigned AranjamenteCuRepetitii(int n, int m)
+{
+    return pow(n,m);
+}
+
+long long unsigned CombinariCuRepetitii(int n, int m)
+{
+    long long unsigned p = 1, q = 1;
+    for(int i = 1; i <= m; i++)
+    {
+        q *= i;  // m!
+        p *= (n + m - i);  // (n + m - 1)!
+    }
+    return p/q;
+}
+
+
+
+int main()
+{
+    system("color E0");
+    system("cls");
     cout << "Proiectul nr. 2 al studentului Ciubara Corneliu, grupa IS11Z. Elemente de combinatorica" << endl << endl;
-    cout << "Elemente din combinatorica" << endl;
-    
+
+    int n, m;
+    cout << "Nr. total: ";
+    cin >> n;
+    cout << "Cate:  ";
+    cin >> m;
     cout << "Factorial iterativ" << endl;
-    for(int j = 0; j <= n; j++) {
+    for(int j=0; j<=n; j++)
+    {
         cout << j << "! = " << FactorialIterativ(j) << endl;
     }
-
-    cout << endl << "Factorial recursiv" << endl;
-    for(int j = 0; j <= n; j++) {
+    cout << endl;
+    cout << "Factorial recursiv" << endl;
+    for(int j=0; j<=n; j++)
+    {
         cout << j << "! = " << FactorialRecursiv(j) << endl;
     }
-
-    cout << "\nPermutari din " << n << " elemente: " << Permutari(n) << endl;
-    cout << "Aranjamente din " << n << " elemente cate " << m << " elemente: " << Aranjamente(n, m) << endl;
-    cout << "Combinari din " << n << " elemente cate " << m << " elemente: " << Combinari(n, m) << endl;
-
+    cout << "Permutari din " << n << " elemente: " << Permutari(n) << endl;
+    cout << "Aranjamente din " << n << " elemente cate " << m << " elemente: " << Aranjamente(n,m) << endl;
+    cout << "Combinari din " << n << " elemente cate " << m << " elemente: " << Combinari(n,m) << endl;
+    cout << "Permutari cu repetitii din " << n << " elemente: " << PermutariCuRepetitii(n) << endl;
+    cout << "Aranjamente cu repetitii din " << n << " elemente cate " << m << " elemente: " << AranjamenteCuRepetitii(n,m) << endl;
+    cout << "Combinari cu repetitii din " << n << " elemente cate " << m << " elemente: " << CombinariCuRepetitii(n,m) << endl;
     return 0;
 }
