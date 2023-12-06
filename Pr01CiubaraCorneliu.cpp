4ef7d6c73b112b58d171c4c7e7a47a6afeedd854
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2023-12-06 14:44:53 +0300

    Update Pr01CiubaraCorneliu.cpp

--- Pr01CiubaraCorneliu.cpp
+++ Pr01CiubaraCorneliu.cpp
@@ -18,74 +18,14 @@ void Line(char lf, char ln, char md, char rt, int nc = 2)
     cout << endl;
 }
 
-// Function for Sheffer stroke (NAND)
-void Sheffer(char lf, char ln, char md, char rt, int nc = 2) {
-    int i;
-    printf("%c", lf);
-    for (int j = 1; j < nc; j++) {
-        for (i = 1; i <= 3; i++)
-            printf("%c", ln);
-        printf("%c", md);
-    }
-    for (i = 1; i <= 7; i++)
-        printf("%c", ln);
-    printf("%c", rt);
-    cout << endl;
-    // Generate truth table for Sheffer stroke
-    printf("%c A %c B %c A | B %c\n", 186, 179, 179, 186);
-    Line(199, 196, 197, 182, 3);
-    for (int a = 0; a <= 1; a++)
-        for (int b = 0; b <= 1; b++)
-            printf("%c %d %c %d %c %3d %3c\n",
-                186, a, 179, b, 179, !(a & b), 186);
-    Line(200, 205, 207, 188, 3);
-}
 
-// Function for Pierce arrow (NOR)
-void Pierce(char lf, char ln, char md, char rt, int nc = 2) {
-    int i;
-    printf("%c", lf);
-    for (int j = 1; j < nc; j++) {
-        for (i = 1; i <= 3; i++)
-            printf("%c", ln);
-        printf("%c", md);
-    }
-    for (i = 1; i <= 7; i++)
-        printf("%c", ln);
-    printf("%c", rt);
-    cout << endl;
-    // Generate truth table for Pierce arrow
-    printf("%c A %c B %c A ^ B %c\n", 186, 179, 179, 186);
-    Line(199, 196, 197, 182, 3);
-    for (int a = 0; a <= 1; a++)
-        for (int b = 0; b <= 1; b++)
-            printf("%c %d %c %d %c %3d %3c\n",
-                186, a, 179, b, 179, !(a | b), 186);
-    Line(200, 205, 207, 188, 3);
-}
 
-// Function for Implication
-void Implication(char lf, char ln, char md, char rt, int nc = 2) {
-    int i;
-    printf("%c", lf);
-    for (int j = 1; j < nc; j++) {
-        for (i = 1; i <= 3; i++)
-            printf("%c", ln);
-        printf("%c", md);
-    }
-    for (i = 1; i <= 7; i++)
-        printf("%c", ln);
-    printf("%c", rt);
-    cout << endl;
-    // Generate truth table for Implication
-    printf("%c A %c B %c A -> B %c\n", 186, 179, 179, 186);
-    Line(199, 196, 197, 182, 3);
-    for (int a = 0; a <= 1; a++)
-        for (int b = 0; b <= 1; b++)
-            printf("%c %d %c %d %c %3d %3c\n",
-                186, a, 179, b, 179, (!a) | b, 186);
-    Line(200, 205, 207, 188, 3);
-}
+
+
+
+
+
+
 
 int main()
 {
@@ -104,11 +44,12 @@ int main()
     printf("%c A %c not A %c\n", 186, 179, 186);
     Line(199, 196, 197, 182);
     for (int a = 0; a <= 1; a++)
-        printf("%c %d %c %3d %3c\n", 186, a, 179, !a, 186);
+        printf("%c %d %c %3d %3c\n",
+               186, a, 179, !a, 186);
     Line(200, 205, 207, 188);
-    //
+cout << endl;
 
-    cout << "\tConjunctia(Intersectie):\n";
+    cout << "\tConjunctia (Intersectie):\n";
     Line(201, 205, 209, 187, 3);
     printf("%c A %c B %cA and B%c\n", 186, 179, 179, 186);
     Line(199, 196, 197, 182, 3);
@@ -117,9 +58,14 @@ int main()
             printf("%c %d %c %d %c %3d %3c\n",
                    186, a, 179, b, 179, a & b, 186);
     Line(200, 205, 207, 188, 3);
+    cout << endl;
     //
 
-    cout << "\tDisjunctia(Reuniunea):\n";
+
+
+
+
+    cout << "\tDisjunctia (Reuniunea):\n";
     Line(201, 205, 209, 187, 3);
     printf("%c A %c B %c A or B%c\n", 186, 179, 179, 186);
     Line(199, 196, 197, 182, 3);
@@ -128,6 +74,7 @@ int main()
             printf("%c %d %c %d %c %3d %3c\n",
                    186, a, 179, b, 179, a | b, 186);
     Line(200, 205, 207, 188, 3);
+    cout << endl;
     //
 
     cout << "\tDiferenta:\n";
@@ -139,7 +86,8 @@ int main()
             printf("%c %d %c %d %c %3d %3c\n",
                    186, a, 179, b, 179, a & !b, 186);
     Line(200, 205, 207, 188, 3);
-    //
+    cout << endl;
+
 
     cout << "\tDiferenta simetrica:\n";
     Line(201, 205, 209, 187, 3);
@@ -150,15 +98,44 @@ int main()
             printf("%c %d %c %d %c %3d %3c\n",
                    186, a, 179, b, 179, a ^ b, 186);
     Line(200, 205, 207, 188, 3);
+cout << endl;
 
-    cout << "\tConectorul Sheffer (NAND):\n";
-    Sheffer(200, 205, 207, 187);
+cout << "\tConectorul Sheffer:\n";
+    Line(201, 205, 209, 187, 3);
+    printf("%c A %c B %cA and B%c\n", 186, 179, 179, 186);
+    Line(199, 196, 197, 182, 3);
+    for (a = 0; a <= 1; a++)
+        for (b = 0; b <= 1; b++)
+            printf("%c %d %c %d %c %3d %3c\n",
+                   186, a, 179, b, 179,!(a & b), 186);
+    Line(200, 205, 207, 188, 3);
+    cout << endl;
 
-    cout << "\tConectorul Pierce (NOR):\n";
-    Pierce(201, 205, 209, 187);
+
+
+cout << "\tConectorul Pierce:\n";
+    Line(201, 205, 209, 187, 3);
+    printf("%c A %c B %cA and B%c\n", 186, 179, 179, 186);
+    Line(199, 196, 197, 182, 3);
+    for (a = 0; a <= 1; a++)
+        for (b = 0; b <= 1; b++)
+            printf("%c %d %c %d %c %3d %3c\n",
+                   186, a, 179, b, 179,!(a | b), 186);
+    Line(200, 205, 207, 188, 3);
+    cout << endl;
+
 
     cout << "\tImplicatia:\n";
-    Implication(201, 205, 209, 187);
+    Line(201, 205, 209, 187, 3);
+    printf("%c A %c B %cA and B%c\n", 186, 179, 179, 186);
+    Line(199, 196, 197, 182, 3);
+    for (a = 0; a <= 1; a++)
+        for (b = 0; b <= 1; b++)
+            printf("%c %d %c %d %c %3d %3c\n",
+                   186, a, 179, b, 179, (!a) | b, 186);
+    Line(200, 205, 207, 188, 3);
+cout << endl;
+
 
     cout << "\tProfesor-Student:\n";
     Line(201, 205, 209, 187, 4);
